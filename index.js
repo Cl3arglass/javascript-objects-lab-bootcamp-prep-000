458e5df9f9eb2a7c628bbac1ecad2caa7359bdab
Holden Altaffer <haltaffer@gmail.com>
2017-11-06 06:47:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 return object;
 }
 
-function deleteFromObjectByKey(object, key) {
-  return delete object.key;
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object
 }
